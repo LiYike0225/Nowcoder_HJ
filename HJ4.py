08e49a353e0f4f3cfac061bada344bf39fe77e55
LiYike0225 <121000454+LiYike0225@users.noreply.github.com>
2024-02-21 19:44:08 +0800

    HJ4.py

--- HJ4.py
+++ HJ4.py
@@ -1,9 +1,10 @@
-str = input()
-length = len(str)
 while True:
     try:
-        for length in range(8):
+        str = input()
+        while len(str) > 8: #若等于8 则会出现 12345678 00000000的情况
             print(str[:8])
-        else
-
+            str = str[8:]
+        print(str.ljust(8,'0')) #ljust用于在不足8个ch的时候补齐
+    except:
+        break
 
